efab422da6757032acfe21f3ea0a0fa64875df17
lucas-perata <contacto.perata@gmail.com>
2022-04-23 14:00:34 -0300

    comentarios post

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-
-  resources :articles
   
   root "articles#index"
 
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show"
+
+  resources :articles do 
+    resources :comments
+  end 
 end
