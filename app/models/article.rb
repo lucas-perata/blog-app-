5466de9870886322adcf322b56749898a990825b
lucas-perata <contacto.perata@gmail.com>
2022-04-24 00:57:13 -0300

    agregado de status a los posteos

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,11 +3,11 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true
 
-    # VALID_STATUSES = ['public', 'private', 'archived']
-    # validates :status, inclusion: {in: VALID_STATUSES}
+    VALID_STATUSES = ['public', 'private', 'archived']
+    validates :status, inclusion: {in: VALID_STATUSES}
 
-    # def archived? 
-    #     status == 'archived'
-    # end
+    def archived? 
+        status == 'archived'
+    end
     
 end
