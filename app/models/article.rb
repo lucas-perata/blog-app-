e466adb0d10dcd3a417093204b3ea4f161e8ac07
lucas-perata <contacto.perata@gmail.com>
2022-04-26 00:38:05 -0300

    autentificacion para acciones determinadas

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
     include Visible 
     
     has_many :comments, dependent: :destroy
+    
     validates :title, presence: true
     validates :body, presence: true
 
